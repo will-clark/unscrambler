6b94927cbe5112efb857538a75d05c044673ed06
Will Clark <will.clark@gmail.com>
2020-10-05 09:10:35 -0400

    spring boot initializr

---
+++ src/main/java/org/willclark/unscrambler/UnscramblerApplication.java
@@ -0,0 +1,13 @@
+package org.willclark.unscrambler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnscramblerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnscramblerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/willclark/unscrambler/UnscramblerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.willclark.unscrambler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnscramblerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
