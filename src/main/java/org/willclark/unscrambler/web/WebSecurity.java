ef49e9b2675d26864bdbe9997372657998268c21
Will Clark <will.clark@gmail.com>
2020-10-13 22:54:13 -0400

    enable cors and disable csrf (for development, not recommended for production)

--- src/main/java/org/willclark/unscrambler/web/WebSecurity.java
+++ src/main/java/org/willclark/unscrambler/web/WebSecurity.java
@@ -1,9 +1,15 @@
 package org.willclark.unscrambler.web;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.NonNull;
 
@@ -15,6 +21,20 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(@NonNull HttpSecurity security) throws Exception {
         security.httpBasic().disable();
         security.formLogin().disable();
+        security.csrf().disable();
+        security.cors();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
