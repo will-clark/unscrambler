2a299f0e3866036b0587cf541788e476545e7d52
Will Clark <will.clark@gmail.com>
2020-10-13 22:45:49 -0400

    adds unique index, rename triggers

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,20 +10,19 @@ CREATE TABLE word (
     chars NVARCHAR(50) NOT NULL,
     length INT NOT NULL
 );
-	
-CREATE TRIGGER on_insert_word ON word
+
+ALTER TABLE word ADD CONSTRAINT u_value UNIQUE(value);
+
+CREATE TRIGGER tgr_insert_word ON word
 FOR INSERT AS 
     UPDATE word 
     SET created = CURRENT_TIMESTAMP, modified = CURRENT_TIMESTAMP, guid = NEWID()
     WHERE word.id = (SELECT id FROM INSERTED);
 GO
 
-CREATE TRIGGER on_update_word ON word
+CREATE TRIGGER tgr_update_word ON word
 FOR UPDATE AS 
     UPDATE word
     SET modified = CURRENT_TIMESTAMP
     WHERE word.id = (SELECT id FROM INSERTED);
 GO
-
-INSERT INTO word (value, chars, length) VALUES ('aaa', 'a', 3);
-INSERT INTO word (value, chars, length) VALUES ('baseball', 'abels', 8);
